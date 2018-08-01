7f7d5981b882f332222ed0749de67616eb3a881c
thyuen <40354641+thyuen@users.noreply.github.com>
2018-08-01 15:17:09 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Melbourne, Australia",
+    -37.8206284,
+    144.9500129
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Incheon, Korea",
+    37.390658,
+    126.6350104
   ]
 ];
